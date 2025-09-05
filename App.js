984895f5e4fe972eb9b8f592c84808113be2a17e
hemanth mg <134934416+hemanthmg20@users.noreply.github.com>
2025-09-05 18:09:58 +0530

    Added CRUD operation from the UI.

--- App.js
+++ App.js
@@ -26,13 +26,14 @@ function Form(){
 
   // Handling input change
   const handleChange = (e) => {
-    setFormData({...formData, [e.target.name] : e.target.value });
+    setFormData({...formData, [e.target.name] : e.target.value }); 
+    // [e.target.name] is a computed property syntax. JS will evaluate this expression and use its result as a key
   };
 
   // Handling form submit
   const handleSubmit = async (e) => {
     e.preventDefault()
-    alert('form submitted');
+    // alert('form submitted');
     console.log(formData);
 
     try{
@@ -55,20 +56,20 @@ function Form(){
 
             <input
               type="text"
-              name="emp_id"
               placeholder="Emplyee ID"
               className="input-field"
               onChange={handleChange}
+              name="emp_id"
               value={formData.emp_id}
               required
             /><br></br>
 
             <input
               type="text"
-              name="name"
               placeholder="Name"
               className="input-field"
               onChange={handleChange}
+              name="name"
               value={formData.name}
               required
             />
@@ -76,10 +77,10 @@ function Form(){
             <br></br>
             <input
             type="text"
-            name="dept"
             placeholder="Department"
             className="input-field"
             onChange={handleChange}
+            name="dept"
             value={formData.dept}
             required
             />
@@ -87,10 +88,10 @@ function Form(){
             <br></br>
             <input
             type="email"
-            name="email"
             placeholder="Email"
             className="input-field"
             onChange={handleChange}
+            name="email"
             value={formData.email}
             required
             />
@@ -98,10 +99,10 @@ function Form(){
             <br></br>
             <input
             type="password"
-            name="password"
             placeholder="password"
             className="input-field"
             onChange={handleChange}
+            name="password"
             value={formData.password}
             required
             />
@@ -120,36 +121,63 @@ function Form(){
 }
 
 function Userlist() {
+  //  State to store the users data
   const [users, setUsers] = useState([]);
 
+  // state to check if we have user in db
+  const [errorResponse, seterrorResponse] = useState(false);
+
+  // to store user being edited 
+  const [editingUser, setEditingUser] = useState(null); 
+  
+// fetching user data
   const userData = async() => {
+    seterrorResponse(false);
     try{
       const response = await axios.get("http://127.0.0.1:8000/emp");
       setUsers(response.data);
       console.log(response.data);
     }catch(error){
-      alert('error fetching users data')
+      if(error.response.status === 404){
+        seterrorResponse(true);
+        // alert("No employees were found");
+      }
+      else{
+        alert('error fetching users data');
+      }
     }
   }
 
+  // deleting the user data
   const deleteUser = async (emp_id) => {
     try {
-      const id = parseInt(emp_id, 10);
-      await axios.delete(`http://127.0.0.1:8000/emp/${(id)}`);
+      // const id = parseInt(emp_id, 10);
+      await axios.delete(`http://127.0.0.1:8000/emp/${emp_id}`);
       alert(`Employee ${emp_id} deleted successfully`);
-      setUsers(users.filter((u) => u.emp_id !== emp_id)); // updates UI
+      setUsers(users.filter((u) => u.emp_id !== emp_id)); // updating state after deletion which intern disappear on the UI
     } catch (error) {
       alert("Failed to delete user");
     }
   };
 
+  // setting editingUser (state object) from null to current user data upon clicking update button so that it conditionally render the update form in the UI
+  const handleEditClick = (user) => {
+    setEditingUser(user);
+  }
+
   return (
-    <div style={{ marginLeft: "170px", marginTop:"30px" }}>
+    <div style={{ display:"flex", flexDirection:"column",alignItems:"center",width:"1000px" }}>
+
       <button onClick={userData} className="submit-btn">
         Get Users
       </button>
 
-      {users.length === 0 ? ('') : 
+      {errorResponse === true ? (
+        <p>No Employees found!</p>
+        ): ('')
+      }
+
+      {users.length === 0  ? ('') : 
       (
         <table border="1" cellPadding="10" style={{ borderCollapse: "collapse", marginTop: "20px" }}>
           <thead>
@@ -166,17 +194,82 @@ function Userlist() {
           <tbody>
               {
                 users.map((user) => (
-                  <UserComp key={user.emp_id} user={user} onDelete={deleteUser}/>
+                  <UserComp key={user.emp_id} user={user} onDelete={deleteUser} onEdit={handleEditClick}/>
                 ))
               }
           </tbody>
         </table>
       )}
+
+      {/*----------- conditonally rendering the update form -------------  */}
+
+      {editingUser && (
+        <div style={{display:"flex", flexDirection:"column",alignItems:"center"}}>
+          <br></br>
+          <h3 style={{color:"gray"}}>Update Employee</h3>
+          <form
+            onSubmit={async (e) => {
+              e.preventDefault();
+              try{
+                const response = await axios.put(`http://127.0.0.1:8000/emp/${editingUser.emp_id}`,editingUser);
+                console.log(response.status);
+              alert('Employee data updated successfully');
+
+              // updating the users state object so that it reflect on the UI
+              setUsers(users.map((u) => u.emp_id === editingUser.emp_id ? editingUser : u));
+
+              // Closing the edit form by setting editingUser back to null
+              setEditingUser(null);
+              }catch(error){
+
+                alert('Falied to update user data')
+              }
+            
+            }}
+          
+          >
+            <input 
+              type="text"
+              value={editingUser.name}
+              onChange={(e) =>
+                setEditingUser({...editingUser,name:e.target.value})
+              }
+              placeholder="Name"
+              className="input-field"
+            /><br></br>
+
+            <input 
+              type="text"
+              value={editingUser.dept}
+              onChange={(e) =>
+                setEditingUser({...editingUser,dept:e.target.value})
+              }
+              placeholder="Department"
+              className="input-field"
+            /><br></br>
+
+            <input
+              type="email"
+              value={editingUser.email}
+              onChange={(e) =>
+                setEditingUser({ ...editingUser, email: e.target.value })
+              }
+              placeholder="Email"
+              className="input-field"
+            /><br></br>
+
+            <button
+             type="submit"
+             className="submit-btn"
+            >Update</button>
+          </form>
+        </div>
+      )}
     </div>
   );
 }
 
-function UserComp({ user, onDelete }) {
+function UserComp({ user, onDelete, onEdit }) {
   return (
     <tr>
       <td>{user.emp_id}</td>
@@ -184,10 +277,10 @@ function UserComp({ user, onDelete }) {
       <td>{user.dept}</td>
       <td>{user.email}</td>
       <td>
-        <button className="update-btn" >Update</button>
+        <button className="update-btn" onClick={() => onEdit(user)} >Update</button>
       </td>
       <td>
-        <button className="delete-btn" onClick={() => onDelete((user.emp_id))}>Delete</button>
+        <button className="delete-btn" onClick={() => onDelete(Number(user.emp_id))}>Delete</button>
       </td>
     </tr>
   );
