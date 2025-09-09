c6631779e27f1692e6ed4d00ae300524d2f6630f
hemanth mg <134934416+hemanthmg20@users.noreply.github.com>
2025-09-09 18:37:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI,Depends,HTTPException
 from sqlalchemy.orm import Session
 from models import empData
 from database import Base,sessionLocal,engine
-from schemas import emp_schema, emp_resopnse, ResetPasswordRequest, emp_patch_schema, emp_put_schema, emp_delete_schema
+from schemas import emp_schema, emp_resopnse, ResetPasswordRequest, emp_patch_schema, emp_put_schema, emp_delete_schema,emp_verify_schema
 from auth import hash_password, verify_password
 
 from fastapi.security import OAuth2PasswordBearer,OAuth2PasswordRequestForm
@@ -89,7 +89,9 @@ def create_emp(request: emp_schema, db: Session = Depends(get_db)):
 
 
 @app.get('/emp', response_model=list[emp_resopnse], tags=["Employees"])
-def get_employees(db: Session = Depends(get_db), current_user: empData = Depends(get_current_user)):
+def get_employees(db: Session = Depends(get_db)
+                #    current_user: empData = Depends(get_current_user)
+                ):
     try:
         emps = db.query(empData).all()
         if not emps:
@@ -115,30 +117,40 @@ def get_emp(id: int, db: Session = Depends(get_db), current_user: empData = Depe
 
 
 @app.put("/emp/reset/{id}", tags=['Employees'])
-def update_passkey(id: int, request: ResetPasswordRequest, db: Session = Depends(get_db), current_user: empData = Depends(get_current_user)):
+def update_passkey(id: int,
+                    request: ResetPasswordRequest,
+                    db: Session = Depends(get_db),
+                    # current_user: empData = Depends(get_current_user)
+                ):
     try:
         emp_data = db.query(empData).filter(empData.emp_id == id).first()
-        if not emp_data:
-            raise HTTPException(status_code=404, detail=f"Employee details with id {id} not found")
+        # if not emp_data:
+        #     raise HTTPException(status_code=404, detail=f"Employee details with id {id} not found")
 
-        if emp_data.email != request.email or not verify_password(request.current_password, emp_data.password):
-            raise HTTPException(status_code=400, detail="Email or current password does not match")
+        # if emp_data.email != request.email or not verify_password(request.current_password, emp_data.password):
+        #     raise HTTPException(status_code=400, detail="Email or current password does not match")
 
-        if verify_password(request.new_password, emp_data.password):
-            raise HTTPException(status_code=400, detail="New password cannot be the same as the old password")
-
-        emp_data.password = hash_password(request.new_password)
-        db.commit()
-        db.refresh(emp_data)
-        return {"detail": "Password reset successful!"}
-    except HTTPException:
-        raise
+        # if verify_password(request.new_password, emp_data.password):
+        #     raise HTTPException(status_code=450, detail="New password cannot be the same as the old password")
+        if (request.new_password == request.confirm_new_password):
+            emp_data.password = hash_password(request.new_password)
+            db.commit()
+            db.refresh(emp_data)
+            return {"detail": "Password reset successful!"}
+    #     else:
+    #         raise HTTPException(status_code=401)
+    # except HTTPException:
+    #     raise
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Unexpected error: {str(e)}")
 
 
 @app.put('/emp/{id}', response_model=emp_resopnse, tags=["Employees"])
-def update_emp(id: int, request: emp_put_schema, db: Session = Depends(get_db), current_user: empData = Depends(get_current_user)):
+def update_emp(id: int,
+                request: emp_put_schema,
+                db: Session = Depends(get_db),
+                # current_user: empData = Depends(get_current_user)
+            ):
     try:
         emp = db.query(empData).filter(empData.emp_id == id).first()
         if not emp:
@@ -178,14 +190,18 @@ def patch_emp(id: int, request: emp_patch_schema, db: Session = Depends(get_db),
 
 
 @app.delete('/emp/{id}', tags=["Employees"])
-def delete_emp(id: int, request: emp_delete_schema, db: Session = Depends(get_db), current_user: empData = Depends(get_current_user)):
+def delete_emp(id: int,
+                # request: emp_delete_schema,
+                db: Session = Depends(get_db), 
+                #current_user: empData = Depends(get_current_user)
+               ):
     try:
         emp = db.query(empData).filter(empData.emp_id == id).first()
         if not emp:
             raise HTTPException(status_code=404, detail=f"Employee details with id {id} not found")
 
-        if emp.email != request.email or not verify_password(request.password, emp.password):
-            raise HTTPException(status_code=400, detail="Email or current password does not match")
+        # if emp.email != request.email or not verify_password(request.password, emp.password):
+        #     raise HTTPException(status_code=400, detail="Email or current password does not match")
 
         db.delete(emp)
         db.commit()
@@ -197,7 +213,10 @@ def delete_emp(id: int, request: emp_delete_schema, db: Session = Depends(get_db
 
 
 @app.post('/emp/verify', tags=["Authenticate user"], include_in_schema=False)
-def verify_emp(request: emp_schema, db: Session = Depends(get_db), current_user: empData = Depends(get_current_user)):
+def verify_emp(request: emp_verify_schema,
+                db: Session = Depends(get_db),
+                # current_user: empData = Depends(get_current_user)
+            ):
     try:
         emp = db.query(empData).filter(empData.emp_id == request.emp_id).first()
         if not emp:
@@ -206,7 +225,7 @@ def verify_emp(request: emp_schema, db: Session = Depends(get_db), current_user:
         if emp.email == request.email and verify_password(request.password, emp.password):
             return {"data": "Validation successful"}
         else:
-            return {"data": "Check your credentials again"}
+            raise HTTPException(status_code=401)
     except HTTPException:
         raise
     except Exception as e:
